2a080849244c6df6a0d5ae0ba8f4fa2ab0abaddc
krlvm <51774833+krlvm@users.noreply.github.com>
2020-09-11 17:25:29 +0300

    Bump version to 1.2.2

--- InstallationForm.Designer.cs
+++ InstallationForm.Designer.cs
@@ -41,10 +41,10 @@
             // 
             // installBtn
             // 
-            this.installBtn.Location = new System.Drawing.Point(11, 355);
-            this.installBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.installBtn.Location = new System.Drawing.Point(15, 437);
+            this.installBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.installBtn.Name = "installBtn";
-            this.installBtn.Size = new System.Drawing.Size(102, 29);
+            this.installBtn.Size = new System.Drawing.Size(136, 36);
             this.installBtn.TabIndex = 0;
             this.installBtn.Text = "Install";
             this.installBtn.UseVisualStyleBackColor = true;
@@ -52,10 +52,10 @@
             // 
             // uninstallBtn
             // 
-            this.uninstallBtn.Location = new System.Drawing.Point(243, 355);
-            this.uninstallBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.uninstallBtn.Location = new System.Drawing.Point(324, 437);
+            this.uninstallBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.uninstallBtn.Name = "uninstallBtn";
-            this.uninstallBtn.Size = new System.Drawing.Size(102, 29);
+            this.uninstallBtn.Size = new System.Drawing.Size(136, 36);
             this.uninstallBtn.TabIndex = 1;
             this.uninstallBtn.Text = "Uninstall";
             this.uninstallBtn.UseVisualStyleBackColor = true;
@@ -65,8 +65,7 @@
             // 
             this.copyrightLabel.AutoSize = true;
             this.copyrightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.copyrightLabel.Location = new System.Drawing.Point(138, 363);
-            this.copyrightLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.copyrightLabel.Location = new System.Drawing.Point(184, 447);
             this.copyrightLabel.Name = "copyrightLabel";
             this.copyrightLabel.Size = new System.Drawing.Size(102, 17);
             this.copyrightLabel.TabIndex = 5;
@@ -75,10 +74,9 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 7);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(382, 30);
+            this.label1.Size = new System.Drawing.Size(448, 34);
             this.label1.TabIndex = 6;
             this.label1.Text = "BeautySearch modifies files of the search application.\r\nRun \"sfc /scannow\" if you" +
     " are unable to open Search after installation.";
@@ -86,10 +84,9 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(9, 329);
-            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.linkLabel1.Location = new System.Drawing.Point(12, 405);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(214, 15);
+            this.linkLabel1.Size = new System.Drawing.Size(250, 17);
             this.linkLabel1.TabIndex = 12;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "https://github.com/krlvm/BeautySearch";
@@ -98,20 +95,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 315);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(12, 388);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(227, 15);
+            this.label2.Size = new System.Drawing.Size(265, 17);
             this.label2.TabIndex = 13;
             this.label2.Text = "Licensed under the GNU GPLv3 License";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 43);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(12, 53);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(182, 45);
+            this.label3.Size = new System.Drawing.Size(213, 51);
             this.label3.TabIndex = 14;
             this.label3.Text = "Tested on Windows 10 versions:\r\n1903, 1909,\r\n2004, 2009.";
             // 
@@ -119,18 +114,18 @@
             // 
             this.featureBox.CheckOnClick = true;
             this.featureBox.FormattingEnabled = true;
-            this.featureBox.Location = new System.Drawing.Point(11, 95);
-            this.featureBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.featureBox.Location = new System.Drawing.Point(15, 117);
+            this.featureBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.featureBox.Name = "featureBox";
-            this.featureBox.Size = new System.Drawing.Size(335, 199);
+            this.featureBox.Size = new System.Drawing.Size(445, 242);
             this.featureBox.TabIndex = 15;
             // 
             // InstallationForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(354, 394);
+            this.ClientSize = new System.Drawing.Size(472, 485);
             this.Controls.Add(this.featureBox);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -141,10 +136,10 @@
             this.Controls.Add(this.installBtn);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "InstallationForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "BeautySearch Installer v1.2.1";
+            this.Text = "BeautySearch Installer v1.2.2";
             this.Load += new System.EventHandler(this.InstallationForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
