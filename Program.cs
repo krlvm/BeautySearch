4d82a76bcf7b3be5e93bc659955743b2fc9786eb
krlvm <51774833+krlvm@users.noreply.github.com>
2021-08-08 21:47:05 +0400

    Add application manifest Declare DPI Awareness in the manifest Require administrator rights to run, fix #13

--- Program.cs
+++ Program.cs
@@ -5,19 +5,12 @@ namespace BeautySearch
 {
     static class Program
     {
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        private static extern bool SetProcessDPIAware();
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            if (Environment.OSVersion.Version.Major >= 6)
-            {
-                SetProcessDPIAware();
-            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new InstallationForm());
