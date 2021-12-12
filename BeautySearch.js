a4d082162811aa1e3b9cc84f4e3bd828dfe170df
krlvm <51774833+krlvm@users.noreply.github.com>
2021-12-12 21:38:07 +0300

    Bump version to 1.8

--- BeautySearch.js
+++ BeautySearch.js
@@ -72,8 +72,7 @@
 // Use defaults if the script is injected manually
 const SETTINGS = SETTINGS_DEFAULTS;
 
-const VERSION = '1.6.3';
-const VERSION_CODE = 15;
+const VERSION = '1.8';
 
 console.log('BeautySearch v' + VERSION + ' is loaded');
 
