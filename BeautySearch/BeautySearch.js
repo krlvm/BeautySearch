1f00c5737910cc53b532573a11f9b3738980ed6a
krlvm <51774833+krlvm@users.noreply.github.com>
2022-05-10 20:29:13 +0300

    Bump version to 1.14.1

--- BeautySearch/BeautySearch.js
+++ BeautySearch/BeautySearch.js
@@ -83,7 +83,7 @@ const SETTINGS_DEFAULTS = {
 // Use defaults if the script is injected manually
 const SETTINGS = SETTINGS_DEFAULTS;
 
-const VERSION = '1.14';
+const VERSION = '1.14.1';
 
 console.log('BeautySearch v' + VERSION + ' is loaded');
 
